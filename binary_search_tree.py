68a5744b8a54d454d3acbb7f145a7758d0c70e32
Vishal <vishal@openspaceservices.com>
2024-11-03 15:53:14 +0530

    dfs, bfs

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -54,3 +54,49 @@ def search(root: TreeNode, target: int):
         return search(root.left, target)
     else:
         return True
+
+
+def inorder(root: TreeNode):
+    if not root:
+        return
+    inorder(root.left)
+    print(root.val)
+    inorder(root.right)
+
+
+def preorder(root: TreeNode):
+    if not root:
+        return
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+
+def postorder(root: TreeNode):
+    if not root:
+        return
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
+
+
+from collections import deque
+
+
+def bfs(root: TreeNode):
+    queue = deque()
+
+    if root:
+        queue.append(root)
+
+    level = 0
+    while len(queue) > 0:
+        print("level: ", level)
+        for i in range(len(queue)):
+            curr = queue.popleft()
+            print(curr.val)
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+        level += 1
